7e6eda43f81f83ac02202c5b656d3edb1d93c775
kiran216 <kirannaidu216@gmail.com>
2017-04-12 09:36:35 +0530

    code changes

--- WebAppForum/Angular/Components/Category/CategoryService.js
+++ WebAppForum/Angular/Components/Category/CategoryService.js
@@ -3,10 +3,10 @@ categoryService.$inject = ['$http', '$q' ];
 
 function categoryService($http, $q) {
 
-    var urlBase = '/api/categories';    
+    var urlBase = 'http://localhost:53758/api/category';
 
     this.getcategories = function () {
-        return $http.get('http://localhost:53758/api/category');
+        return $http.get(urlBase);
     };
     this.getcategory = function (id) {
         return $http.get(urlBase + '/' + id);
@@ -24,5 +24,31 @@ function categoryService($http, $q) {
         return $http.delete(urlBase + '/' + id);
     };
 
+    this.showAddCategoryForm = function (elementId) {
+        $("." + elementId + "").show();
+        $(".edit-content").slideUp("slow");
+        $(".add-content").slideDown("slow");
+       this.focusToElement(elementId);
+    };
+    this.showEditCategoryForm = function (elementId) {
+        $("." + elementId + "").show();
+        $(".add-content").slideUp("slow");
+        $(".edit-content").slideDown("slow");
+        this.focusToElement(elementId);
+    };
+    this.focusToElement= function (elementId) {
+        $('html, body').animate({ scrollTop: $('#' + elementId).position().top }, 'slow');
+    };
+    this.focusToTop= function () {
+        $('html, body').animate({ scrollTop: 0 }, 800);
+    };
+    this.resetPage = function () {
+        $(".add-content").slideUp("slow");
+        $(".edit-content").slideUp("slow");
+        $(".alert").hide();
+        $(".modal-header.alert.alert-success").show();
+        $("." + elementId + "").hide();
+        this.focusToTop();
+    };
 
 }
